ad5dcb79dc105ee6c774cc165ae540b843c70070
KentPhan <kent.chi.lam@gmail.com>
2019-09-17 11:09:22 -0600

    First commit of the unreal project.

---
+++ IslandMainProject/Source/IslandMainProject/IslandMainProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "IslandMainProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IslandMainProject, "IslandMainProject" );
