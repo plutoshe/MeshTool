bf791ab29dd1d29aeb00caae09e82fa399b75cac
HUANG Bocheng <a923000213@163.com>
2019-09-18 20:45:43 -0600

    Migrate Player Controller, Player UI
    1. Fix Pickup item bugs

---
+++ IslandMainProject/Source/IslandMainProject/Private/PlayerControllerBase.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Public/PlayerControllerBase.h"
+
+APlayerControllerBase::APlayerControllerBase()
+{
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Crosshairs;
+}
